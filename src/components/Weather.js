92696bb8612f518f6a10e39f62aa3d393c596fcd
kawarimidoll <kawarimidoll+git@gmail.com>
2020-11-06 21:10:04 +0900

    use useReducer instead of useState

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,30 +1,68 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 
 const CITY_NAME = "Tokyo";
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${CITY_NAME},jp&APPID=${API_KEY}&units=metric`;
 
+const initialState = {
+  loading: true,
+  message: "",
+  weather: [],
+  main: {},
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "REQUEST":
+      return {
+        ...state,
+        loading: true,
+        message: "",
+      };
+    case "SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        weather: action.weather,
+        main: action.main,
+      };
+    case "FAILURE":
+      return {
+        ...state,
+        loading: false,
+        message: action.message,
+      };
+    default:
+      return state;
+  }
+};
+
 const Weather = () => {
-  const [loading, setLoading] = useState(true);
-  const [result, setResult] = useState({});
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
+    dispatch({ type: "REQUEST" });
+
     fetch(API_URL)
       .then((response) => response.json())
       .then((jsonResponse) => {
-        setResult(jsonResponse);
-        setLoading(false);
+        dispatch({
+          type: jsonResponse.cod === 200 ? "SUCCESS" : "FAILURE",
+          ...jsonResponse,
+        });
       });
   }, []);
 
-  const { weather, main, wind, clouds } = result;
-  console.log(weather, main, wind, clouds);
+  const { loading, weather, main, message } = state;
+  // console.log(weather, main, wind, clouds);
 
   return (
     <div>
-      <h2>{CITY_NAME} </h2>
+      <h2>{CITY_NAME}</h2>
       {loading ? (
         <div>loading...</div>
+      ) : message ? (
+        <div>{message}</div>
       ) : (
         <div>
           <div>
@@ -36,7 +74,7 @@ const Weather = () => {
             <h3>{weather[0].main}</h3>
           </div>
           <div>
-            {main.temp_max} - {main.temp_min}
+            {main.temp_max}℃ - {main.temp_min}℃
           </div>
         </div>
       )}
