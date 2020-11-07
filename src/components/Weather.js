2e35d259fbabfd26b776d3ad3300347868517a86
kawarimidoll <kawarimidoll+git@gmail.com>
2020-11-07 11:07:38 +0900

    fix loop part

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -49,8 +49,8 @@ const Weather = () => {
         <div>{message}</div>
       ) : current.weather ? (
         <div>
-          {current.weather.map((weather) => (
-            <div>
+          {current.weather.map((weather, index) => (
+            <div key={`weather-${index}`}>
               <img
                 src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`}
                 alt={weather.description}
